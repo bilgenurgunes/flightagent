4d9aace01963a3f96add6575aeb190f14d0a21c0
bnrgns2022 <gunesbilgenur3577@gmail.com>
2022-08-23 16:36:08 +0300

    first commit

---
+++ src/test/java/com/deneme/planlamadeneme/PlanlamadenemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deneme.planlamadeneme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanlamadenemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
